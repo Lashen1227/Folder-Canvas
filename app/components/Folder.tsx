ce27b717e71f163e57839d2bfd6ae2896f49d231
Lashen1227 <lashenmartino@gmail.com>
2025-07-10 21:22:46 +0530

    fix: allow null for canvasRef in Folder component props

--- app/components/Folder.tsx
+++ app/components/Folder.tsx
@@ -3,21 +3,23 @@ import Image from 'next/image'
 import fallbackFolder from '../../public/fallback-folder.png'
 import { RefObject } from 'react'
 
-export function Folder({
-   loading,
-   canvasRef,
-}: {
+interface FolderProps {
    loading: boolean
-   canvasRef: RefObject<HTMLCanvasElement>
-}) {
+   canvasRef: RefObject<HTMLCanvasElement | null> // <-- Fix: allow null
+}
+
+export function Folder({ loading, canvasRef }: FolderProps) {
    return (
       <>
-         <canvas className={loading ? 'hidden' : 'w-[512px] h-[512px]'} ref={canvasRef}></canvas>
+         <canvas
+            className={loading ? 'hidden' : 'w-[512px] h-[512px]'}
+            ref={canvasRef}
+         ></canvas>
 
          {loading && (
             <Image
                src={fallbackFolder}
-               alt='macOS folder icon '
+               alt='macOS folder icon'
                width={512}
                height={512}
                priority
